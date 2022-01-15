e94b9b8d8501dbcdb2b5c7b9254f85f24ef3b119
VnHerman <96006850+VnHerman@users.noreply.github.com>
2022-01-15 14:01:18 +0700

    yeu anh nhe

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Yêu Anh Nhé 🥰",
+    introTitle: 'Ly(Lea)',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Làm người yêu anh nhé?^^🥰',
+    desc: 'Nếu em thoát ra mà không trả lời thì có nghĩa là em đồng ý rồi nhé^^',
+    btnYes: 'Yêu anh<33',
+    btnNo: 'Không yêu anh đâu :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu anh <33333333',
+    mess: 'Anh biết mà 🥰. Yêu Ly nhiều nhiều 😘😘',
+    messDesc: 'Vậy em chính thức làm người yêu của anh rồi đó😘',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100072981851698' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
